ab75168b7d1b0724db11fef238d0262a848e7e5f
Bruno-Regis <149000097+Bruno-Regis@users.noreply.github.com>
2025-09-24 19:56:06 -0300

    Complete Members CRUD

---
+++ GymManagement/Controllers/MembersController.cs
@@ -0,0 +1,108 @@
+﻿using GymManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace GymManagement.Controllers
+{
+    public class MembersController :  Controller
+    {
+        private readonly AppDbContext _context;
+        public MembersController(AppDbContext context)
+        {
+            _context = context;
+        }
+         
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Members.ToListAsync();
+            return View(data);
+        }
+
+        public IActionResult Create() // Show Form
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Member member) // Recieve data via post
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Members.Add(member);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(member);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var data = await _context.Members.FindAsync(id);
+
+            if (data == null)
+                return NotFound();
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Member member)
+        {
+            if (id != member.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Members.Update(member);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var data = await _context.Members.FindAsync(id);
+
+            if (data == null)
+                return NotFound();
+
+            return View(data);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var data = await _context.Members.FindAsync(id);
+
+            if (data == null)
+                return NotFound();
+
+            return View(data);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var data = await _context.Members.FindAsync(id);
+
+            if (data == null)
+                return NotFound();
+
+            _context.Members.Remove(data);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+} 
--- GymManagement/Models/Member.cs
+++ GymManagement/Models/Member.cs
@@ -11,12 +11,15 @@ namespace GymManagement.Models
         [Required(ErrorMessage = "Required field")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Required field")]
+        [Display(Name ="Birth Date")]
         public DateTime BirthDate { get; set; }
         [Required(ErrorMessage = "Required field")]
         public string Email { get; set; }
         [Phone]
+        [Display(Name ="Phone")]
         public string PhoneNumber { get; set; }
         [ForeignKey("Plan")]
+        [Display(Name ="Subscribed Plan")]
         public string SubscribedPlanId { get; set; }
 
     }
